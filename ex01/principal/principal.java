966cd3b581ec1f2c875968f341468c044a2dd5b9
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-05-01 23:11:38 -0300

    Update principal.java

--- ex01/principal/principal.java
+++ ex01/principal/principal.java
@@ -4,8 +4,8 @@ import entidades.Colaborador;
 import entidades.Departamento;
 
 public class principal {
-	public static void main(String[] args) {
-        Colaborador colaborador = new Colaborador(1, "Carlos", 25, "Analista de sistemas");
+    public static void main(String[] args) {
+        Colaborador colaborador = new Colaborador(1, "Carlos", 25, "Analista de Sistemas");
         Departamento departamento = new Departamento("TI", "TID", 20, colaborador);
         departamento.exibirInformacoes();
     }
