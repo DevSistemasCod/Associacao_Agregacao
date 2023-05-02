2e457ed5434cb88c7e2ed1dc8ac61ce5ea595217
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-05-01 23:25:01 -0300

    Update Principal.java

--- ex02/principal/Principal.java
+++ ex02/principal/Principal.java
@@ -4,7 +4,7 @@ import entidades.Endereco;
 import entidades.Pessoa;
 
 public class Principal {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Endereco endereco = new Endereco("São Carlos", "Centro", "Av: São Carlos ", 123, 12345678, "São Paulo", "Brasil");
         Pessoa pessoa = new Pessoa("Carlos", 23, 123456, endereco);
         System.out.println(pessoa);
